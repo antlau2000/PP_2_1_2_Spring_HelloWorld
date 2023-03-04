d78fa3846a6eeed423d73ace1d6fbe88763ac63d
antlau2000 <61512568+antlau2000@users.noreply.github.com>
2023-03-04 17:32:30 +0300

    add Cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,13 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld helloWorld =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld helloWorld1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat = applicationContext.getBean("cat", Cat.class);
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+        System.out.println("HelloWorld = " + (helloWorld == helloWorld1) + "\n"
+                + "Cat = " + (cat == cat1));
     }
 }
